e4f037cc5cdbd3dff238ef291ab9f8b96ae016a4
VISHAL <vishalsojitra.dh@gmail.com>
2017-04-14 13:19:36 +0530

    Initial commit

---
+++ library/src/main/java/com/example/library/OnItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.library;
+
+import android.view.View;
+
+/**
+ * Created by VISHAL on 4/12/2017.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(View view, int position);
+}
